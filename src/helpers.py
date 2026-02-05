1dd41a65f9ff8a1e1fec9e98c39a1b0091cf1c3c
levy-neo <164898827+levy-neo@users.noreply.github.com>
2026-02-05 10:07:05 -0300

    (fix) - Modificar helpers.gerar_id

--- src/helpers.py
+++ src/helpers.py
@@ -1,27 +1,24 @@
 import hashlib
 import pandas as pd
 
-def gerar_id(df, colunas, nome_coluna='id_validacao', prefixo=None, tamanho=10):
+def gerar_id(text,  prefixo=None, tamanho=10):
     """
-    Gera um ID determinístico baseado em colunas de um DataFrame.
+    Gera um ID determinístico baseado em uma lista de valores.
 
     Args:
-        df (pd.DataFrame): DataFrame com os dados.
-        colunas (list): Lista de colunas que serão combinadas para gerar o ID.
-        nome_coluna (str): Nome da nova coluna com o ID.
+        text: texto enviado que será passando pelo hash.
         prefixo (str, opcional): Prefixo opcional para o ID.
         tamanho (int): Tamanho do hash gerado.
 
     Returns:
-        pd.DataFrame: DataFrame com a nova coluna de ID.
+        hash gerado
+        
     """
-    def gerar_hash(row):
-        texto = '|'.join(str(row[col]).strip().lower() if pd.notna(row[col]) else '' for col in colunas)
-        hash_valor = hashlib.sha256(texto.encode()).hexdigest()[:tamanho]
-        return f"{prefixo}_{hash_valor}" if prefixo else hash_valor
+    
+    texto = '|'.join(text).strip().lower() if text is not None else ''
+    hash_valor = hashlib.sha256(texto.encode()).hexdigest()[:tamanho]
 
-    df[nome_coluna] = df.apply(gerar_hash, axis=1)
-    return df
+    return f"{prefixo}_{hash_valor}" if prefixo else hash_valor
 
 def tratamento_cnpj(cnpj: str) -> str:
     return ''.join(filter(str.isdigit, cnpj))
